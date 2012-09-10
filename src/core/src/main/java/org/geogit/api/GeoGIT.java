8264ce35e1a519d1f51a10ea51f439f6b44ffbf3
Gabriel Roldan <gabriel.roldan@gmail.com>
2012-09-10 17:14:45 -0300

    open the repository before returning it or the dbs end up being not initialized

--- src/core/src/main/java/org/geogit/api/GeoGIT.java
+++ src/core/src/main/java/org/geogit/api/GeoGIT.java
@@ -114,6 +114,7 @@ public class GeoGIT {
         if (repoFound) {
             try {
                 repository = injector.getInstance(Repository.class);
+                repository.create();
             } catch (Exception e) {
                 throw Throwables.propagate(e);
             }
