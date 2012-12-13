a1752c79e3403c32f5e731bbf11a81eb07f18e27
Ian Schneider <planablediglet@gmail.com>
2012-12-13 11:37:12 -0700

    change to subclass of illegalargumentexc

--- src/web/src/main/java/org/geogit/web/api/CommandSpecException.java
+++ src/web/src/main/java/org/geogit/web/api/CommandSpecException.java
@@ -4,7 +4,7 @@ package org.geogit.web.api;
  * A user-input (or lack thereof) driven exception. Purposefully does not have
  * a constructor to allow a Throwable cause to be specified.
  */
-public class CommandSpecException extends RuntimeException {
+public class CommandSpecException extends IllegalArgumentException {
 
     public CommandSpecException(String message) {
         super(message);
