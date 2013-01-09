b5505da734e29d25ad5f3ad5ec2c7430c2b00dc2
Kelsey Ishmael <kelsey.ishmael@lmnsolutions.com>
2013-01-09 18:13:43 -0500

    Working on manifest formatting.

--- src/web/src/main/java/org/geogit/web/api/repo/ManifestResource.java
+++ src/web/src/main/java/org/geogit/web/api/repo/ManifestResource.java
@@ -2,18 +2,22 @@ package org.geogit.web.api.repo;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Map;
 
 import org.geogit.api.GeoGIT;
+import org.geogit.api.Ref;
+import org.geogit.api.plumbing.RefParse;
+import org.geogit.api.porcelain.BranchListOp;
 import org.restlet.data.MediaType;
 import org.restlet.representation.WriterRepresentation;
 import org.restlet.resource.ServerResource;
+
+import com.google.common.collect.ImmutableList;
 
 public class ManifestResource extends ServerResource {
     {
         getVariants().add(new ManifestRepresentation());
     }
-    
+
     private class ManifestRepresentation extends WriterRepresentation {
         public ManifestRepresentation() {
             super(MediaType.TEXT_PLAIN);
@@ -22,11 +26,19 @@ public class ManifestResource extends ServerResource {
         @Override
         public void write(Writer w) throws IOException {
             GeoGIT ggit = (GeoGIT) getApplication().getContext().getAttributes().get("geogit");
-            Map<String, String> refs = ggit.getRepository().getRefDatabase().getAll();
-            for (Map.Entry<String, String> entry : refs.entrySet()) {
-                w.write(entry.getKey());
+            ImmutableList<Ref> refs = ggit.command(BranchListOp.class).call();
+            // Print out HEAD first
+            final Ref currentHead = ggit.command(RefParse.class).setName(Ref.HEAD).call().get();
+            w.write("HEAD: ");
+            w.write(currentHead.getName());
+            w.write(" ");
+            w.write(currentHead.getObjectId().toString());
+            w.write("\n");
+            // Print out the local branches
+            for (Ref ref : refs) {
+                w.write(ref.getName());
                 w.write(" ");
-                w.write(entry.getValue());
+                w.write(ref.getObjectId().toString());
                 w.write("\n");
             }
             w.flush();
