381ec075ad76543e5759229a7e2cfef14af6199e
Gabriel Roldan <gabriel.roldan@gmail.com>
2013-02-01 19:19:51 -0300

    compose a default commit message at GeogitTransactionState.commit if none is provided

--- src/geotools/src/main/java/org/geogit/geotools/data/GeogitTransactionState.java
+++ src/geotools/src/main/java/org/geogit/geotools/data/GeogitTransactionState.java
@@ -6,12 +6,15 @@
 package org.geogit.geotools.data;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import javax.annotation.Nullable;
 
 import org.geogit.api.CommandLocator;
 import org.geogit.api.GeogitTransaction;
+import org.geogit.api.plumbing.DiffIndex;
 import org.geogit.api.plumbing.TransactionBegin;
+import org.geogit.api.plumbing.diff.DiffEntry;
 import org.geogit.api.porcelain.AddOp;
 import org.geogit.api.porcelain.CheckoutOp;
 import org.geogit.api.porcelain.CommitOp;
@@ -92,16 +95,17 @@ class GeogitTransactionState implements State {
          * org.geoserver.wfs.Transaction.getDatastoreTransaction()
          */
         final String author = (String) this.tx.getProperty(VERSIONING_COMMIT_AUTHOR);
-        final String commitMessage = (String) this.tx.getProperty(VERSIONING_COMMIT_MESSAGE);
+        String commitMessage = (String) this.tx.getProperty(VERSIONING_COMMIT_MESSAGE);
         this.geogitTx.command(AddOp.class).call();
         try {
             CommitOp commitOp = this.geogitTx.command(CommitOp.class);
             if (author != null) {
                 commitOp.setAuthor(author, null);
             }
-            if (commitMessage != null) {
-                commitOp.setMessage(commitMessage);
+            if (commitMessage == null) {
+                commitMessage = composeDefaultCommitMessage();
             }
+            commitOp.setMessage(commitMessage);
             commitOp.call();
         } catch (NothingToCommitException nochanges) {
             // ok
@@ -113,6 +117,55 @@ class GeogitTransactionState implements State {
                 dataStore.getOrFigureOutBranch());
 
         this.geogitTx = null;
+    }
+
+    private String composeDefaultCommitMessage() {
+        Iterator<DiffEntry> indexDiffs = this.geogitTx.command(DiffIndex.class).call();
+        int added = 0, removed = 0, modified = 0;
+        StringBuilder msg = new StringBuilder();
+        while (indexDiffs.hasNext()) {
+            DiffEntry entry = indexDiffs.next();
+            switch (entry.changeType()) {
+            case ADDED:
+                added++;
+                break;
+            case MODIFIED:
+                modified++;
+                break;
+            case REMOVED:
+                removed++;
+                break;
+            }
+            if ((added + removed + modified) < 10) {
+                msg.append("\n ").append(entry.changeType().toString().toLowerCase()).append(" ")
+                        .append(entry.newPath() == null ? entry.oldName() : entry.newPath());
+            }
+        }
+        int count = added + removed + modified;
+        if (count > 10) {
+            msg.append("\n And ").append(count - 10).append(" more changes.");
+        }
+        StringBuilder title = new StringBuilder();
+        if (added > 0) {
+            title.append("added ").append(added);
+        }
+        if (modified > 0) {
+            if (title.length() > 0) {
+                title.append(", ");
+            }
+            title.append("modified ").append(modified);
+        }
+        if (removed > 0) {
+            if (title.length() > 0) {
+                title.append(", ");
+            }
+            title.append("removed ").append(removed);
+        }
+        if (count > 0) {
+            title.append(" features via unversioned legacy client.\n");
+        }
+        msg.insert(0, title);
+        return msg.toString();
     }
 
     @Override
