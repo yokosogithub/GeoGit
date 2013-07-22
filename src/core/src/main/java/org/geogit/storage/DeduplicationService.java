29a0a059617dc032e80700a65de5bb9f98f749c0
David Winslow <dwinslow@opengeo.org>
2013-07-22 13:19:59 -0400

    Add javadoc comment to abstract interfaces

--- src/core/src/main/java/org/geogit/storage/DeduplicationService.java
+++ src/core/src/main/java/org/geogit/storage/DeduplicationService.java
@@ -1,5 +1,21 @@
 package org.geogit.storage;
 
+/**
+ * A service for providing deduplicators.
+ */
 public interface DeduplicationService {
+    /**
+     * Create a new Deduplicator.  Clients MUST ensure that the deduplicator's
+     * release() method is called.  For example:
+     *
+     * <code>
+     *   Deduplicator deduplicator = deduplicationService().createDeduplicator();
+     *   try {
+     *       client.use(deduplicator);
+     *   } finally {
+     *       deduplicator.release();
+     *   }
+     * </code>
+     */
     Deduplicator createDeduplicator();
 }
