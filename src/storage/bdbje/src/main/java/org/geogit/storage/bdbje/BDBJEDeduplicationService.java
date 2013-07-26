9f59b348621c4633c7aa3d28864fb93d836e174a
Gabriel Roldan <gabriel.roldan@gmail.com>
2013-07-26 20:37:19 -0300

    Add javadocs to BDB JE Deduplicator/Service and avoid a synchronization by using an AtomicInteger as the tick used for the database name.

--- src/storage/bdbje/src/main/java/org/geogit/storage/bdbje/BDBJEDeduplicationService.java
+++ src/storage/bdbje/src/main/java/org/geogit/storage/bdbje/BDBJEDeduplicationService.java
@@ -6,6 +6,7 @@ package org.geogit.storage.bdbje;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.geogit.storage.DeduplicationService;
 import org.geogit.storage.Deduplicator;
@@ -15,46 +16,61 @@ import com.sleepycat.je.Database;
 import com.sleepycat.je.DatabaseConfig;
 import com.sleepycat.je.Environment;
 
+/**
+ * A {@link DeduplicationService} that creates {@link DatabaseConfig#setTemporary(boolean)
+ * temporary} BDB JE databases in the {@code .geogit/seen} environment upon every
+ * {@link #createDatabase()} call.
+ * <p>
+ * The created temporary databases share the heap cache with the other JE databases, namely the
+ * {@link JEObjectDatabase object} and {@link JEStagingDatabase staging} databases, and will page to
+ * disk as appropriate when the BDB JE cache is full.
+ * </p>
+ * 
+ * @see BDBJEDeduplicator
+ */
 public class BDBJEDeduplicationService implements DeduplicationService {
     private EnvironmentBuilder environmentBuilder;
+
     private Set<BDBJEDeduplicator> openDeduplicators = new HashSet<BDBJEDeduplicator>();
+
     private volatile Environment environment;
-    private volatile int tick = 0;
+
+    private volatile AtomicInteger tick = new AtomicInteger();
 
     @Inject
     public BDBJEDeduplicationService(EnvironmentBuilder environmentBuilder) {
         this.environmentBuilder = environmentBuilder;
     }
-    
+
     private synchronized Environment getEnvironment() {
         if (this.environment == null) {
             this.environment = environmentBuilder.setRelativePath("seen").get();
         }
         return this.environment;
     }
-    
-    private synchronized Database createDatabase() {
+
+    private Database createDatabase() {
         DatabaseConfig dbConfig = new DatabaseConfig();
         dbConfig.setAllowCreate(true);
         dbConfig.setDeferredWrite(false);
         dbConfig.setTransactional(false);
         dbConfig.setTemporary(true);
 
-        return getEnvironment().openDatabase(null, "seen" + (tick++), dbConfig);
+        return getEnvironment().openDatabase(null, "seen" + tick.incrementAndGet(), dbConfig);
     }
-    
+
     @Override
-    public synchronized Deduplicator createDeduplicator() {
-    	Database database = createDatabase();
+    public Deduplicator createDeduplicator() {
+        Database database = createDatabase();
         BDBJEDeduplicator deduplicator = new BDBJEDeduplicator(database, this);
         this.openDeduplicators.add(deduplicator);
         return deduplicator;
     }
-    
+
     protected void reset(BDBJEDeduplicator deduplicator) {
-    	deduplicator.setDatabase(createDatabase());
+        deduplicator.setDatabase(createDatabase());
     }
-    
+
     public synchronized void deregister(BDBJEDeduplicator deduplicator) {
         this.openDeduplicators.remove(deduplicator);
         if (this.openDeduplicators.size() == 0) {
