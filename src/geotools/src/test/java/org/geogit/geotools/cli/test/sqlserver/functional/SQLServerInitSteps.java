8ec9b0117c5493a4403bc5a899505dc9bae6b1f4
Gabriel Roldan <gabriel.roldan@gmail.com>
2013-10-12 19:17:20 -0300

    Add missing (c) headers

--- src/geotools/src/test/java/org/geogit/geotools/cli/test/sqlserver/functional/SQLServerInitSteps.java
+++ src/geotools/src/test/java/org/geogit/geotools/cli/test/sqlserver/functional/SQLServerInitSteps.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2013 OpenPlans. All rights reserved.
+ * This code is licensed under the BSD New License, available at the root
+ * application directory.
+ */
 package org.geogit.geotools.cli.test.sqlserver.functional;
 
 import static org.geogit.cli.test.functional.GlobalState.currentDirectory;
