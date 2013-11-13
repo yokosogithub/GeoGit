3b02f44e0257811bec3f83905c087fc461a5c368
David Winslow <dwinslow@opengeo.org>
2013-11-13 14:05:46 -0500

    Use Singleton scope for Neo4JGraphDatabase
    Fixes #493

--- src/storage/neo4j/src/main/java/org/geogit/storage/neo4j/Neo4JModule.java
+++ src/storage/neo4j/src/main/java/org/geogit/storage/neo4j/Neo4JModule.java
@@ -7,12 +7,11 @@ package org.geogit.storage.neo4j;
 import org.geogit.storage.GraphDatabase;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 public class Neo4JModule extends AbstractModule {
-
     @Override
     protected void configure() {
-        bind(GraphDatabase.class).to(Neo4JGraphDatabase.class);
+        bind(GraphDatabase.class).to(Neo4JGraphDatabase.class).in(Scopes.SINGLETON);
     }
-
 }
