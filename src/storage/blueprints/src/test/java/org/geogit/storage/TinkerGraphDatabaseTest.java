a749d55d3d14833a3569a74763abdc2e49a6b232
jdeolive <jdeolive@boundlessgeo.com>
2013-12-10 20:09:07 -0700

    Refactored out blueprints dependency from core module.
    - Created dedicated blueprints module for blueprints graph base classes
    - Reimplemented HeapGraphDatabase without dependency on blueprints
    - Added HeapGraphDatabaseStressTest

--- src/storage/blueprints/src/test/java/org/geogit/storage/TinkerGraphDatabaseTest.java
+++ src/storage/blueprints/src/test/java/org/geogit/storage/TinkerGraphDatabaseTest.java
@@ -7,15 +7,24 @@ package org.geogit.storage;
 import org.geogit.di.GeogitModule;
 import org.junit.Assert;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.util.Modules;
+
 
 public class TinkerGraphDatabaseTest extends GraphDatabaseTest {
 
     @Override
     protected Injector createInjector() {
-        Injector injector = Guice.createInjector(new GeogitModule()); // relies on TinkerGraph being
-                                                                      // default
+        Injector injector = Guice.createInjector(Modules.override(new GeogitModule()).with(
+            new AbstractModule() {
+                @Override
+                protected void configure() {
+                    bind(GraphDatabase.class).to(TinkerGraphDatabase.class);
+                }
+            }));
+        
         Assert.assertTrue(injector.getInstance(GraphDatabase.class) instanceof TinkerGraphDatabase);
         return injector;
     }
