dwins/GeoGig

Add support for using different formats from the command line tool Start on plugin support
Interim commit

Interim commit

Get tests passing

src/cli/src/main/java/org/geogit/cli/porcelain/Init.java
@@ -23,6 +23,7 @@ import org.geogit.repository.Repository;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.google.common.collect.ImmutableList;
 
 /**
  * This command creates an empty geogit repository - basically a .geogit directory with
@@ -44,6 +45,19 @@ public class Init extends AbstractCommand implements CLICommand {
 
     @Parameter(description = "Repository location (directory).", required = false, arity = 1)
     private List<String> location;
+
+    @Parameter(names={ "-c", "--config" }, description = "Configuration options for new repository", required = false, variableArity=true)
+    private List<String> configuration;
+
+    private final static List<String> defaultConfiguration =
+        ImmutableList.of(
+                "storage.graph", "tinkergraph",
+                "tinkergraph.version", "0.1",
+                "storage.objects", "bdbje",
+                "storage.staging", "bdbje",
+                "bdbje.version", "0.1",
+                "storage.refs", "file",
+                "file.version", "1.0");
 
     /**
      * Executes the init command.
@@ -77,7 +91,12 @@ public class Init extends AbstractCommand implements CLICommand {
             geogit = cli.getGeogit();
         }
 
-        Repository repository = geogit.command(InitOp.class).call();
+        ImmutableList.Builder<String> builder = ImmutableList.builder();
+        builder.addAll(defaultConfiguration);
+        if (configuration != null) builder.addAll(configuration);
+        List<String> effectiveConfiguration = builder.build();
+
+        Repository repository = geogit.command(InitOp.class).setInitialOptions(effectiveConfiguration).call();
         final boolean repoExisted = repository == null;
         geogit.setRepository(repository);
         cli.setGeogit(geogit);
@@ -98,6 +117,5 @@ public class Init extends AbstractCommand implements CLICommand {
             message = "Initialized empty Geogit repository in " + repoDirectory.getAbsolutePath();
         }
         cli.getConsole().println(message);
-
     }
 }

yokosogithub/GeoGit

src/cli/src/main/java/org/geogit/cli/porcelain/Init.java
@@ -25,6 +25,7 @@ import org.geogit.repository.Repository;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.google.common.collect.ImmutableList;
 
 /**
  * This command creates an empty geogit repository - basically a .geogit directory with
@@ -49,6 +50,16 @@ public class Init extends AbstractCommand implements CLICommand {
 
     @Parameter(names = "--config", description = "Initial configuration values for new repository", required = false, variableArity = true)
     private List<String> config;
+
+    private final static List<String> defaultConfiguration =
+        ImmutableList.of(
+                "storage.graph", "tinkergraph",
+                "tinkergraph.version", "0.1",
+                "storage.objects", "bdbje",
+                "storage.staging", "bdbje",
+                "bdbje.version", "0.1",
+                "storage.refs", "file",
+                "file.version", "1.0");
 
     /**
      * Executes the init command.
@@ -85,12 +96,12 @@ public class Init extends AbstractCommand implements CLICommand {
             geogit = cli.getGeogit();
         }
 
-        Repository repository;
-        try {
-            repository = geogit.command(InitOp.class).setConfig(config).call();
-        } catch (ConfigException e) {
-            throw new CommandFailedException("Couldn't apply provided configuration: " + e.statusCode);
-        }
+        ImmutableList.Builder<String> builder = ImmutableList.builder();
+        builder.addAll(defaultConfiguration);
+        if (config != null) builder.addAll(config);
+        List<String> effectiveConfiguration = builder.build();
+
+        Repository repository = geogit.command(InitOp.class).setConfig(effectiveConfiguration).call();
         final boolean repoExisted = repository == null;
         geogit.setRepository(repository);
         cli.setGeogit(geogit);