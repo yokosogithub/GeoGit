55830dfa72a73e6f0278f9e276ac942bd434c3a5
Gabriel Roldan <gabriel.roldan@gmail.com>
2014-03-11 10:50:59 -0300

    Add getTempDir() method to org.geogit.api.Platform

--- src/core/src/main/java/org/geogit/api/DefaultPlatform.java
+++ src/core/src/main/java/org/geogit/api/DefaultPlatform.java
@@ -8,7 +8,16 @@ package org.geogit.api;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.TimeZone;
+
+import org.geogit.api.plumbing.ResolveGeogitDir;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
 
 /**
  * Standard platform for GeoGit.
@@ -72,6 +81,26 @@ public class DefaultPlatform implements Platform {
     @Override
     public long nanoTime() {
         return System.nanoTime();
+    }
+
+    @Override
+    public File getTempDir() {
+        Optional<URL> url = new ResolveGeogitDir(this).call();
+        final File tmpDir;
+        if (url.isPresent()) {
+            try {
+                URI uri = url.get().toURI();
+                tmpDir = new File(new File(uri), "tmp");
+                Preconditions.checkState(tmpDir.exists() || tmpDir.mkdir(),
+                        "unable to create directory %s", tmpDir.getAbsolutePath());
+            } catch (URISyntaxException e) {
+                throw Throwables.propagate(e);
+            }
+        } else {
+            String systmplocation = System.getProperty("java.io.tmpdir");
+            tmpDir = new File(systmplocation);
+        }
+        return tmpDir;
     }
 
     @Override
