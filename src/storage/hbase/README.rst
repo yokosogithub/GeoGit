8f16d426c83da948b19b00821ebe6a52f2eb8d9b
Andy Wang <andymw91@gmail.com>
2014-03-13 22:26:46 -0400

    first commit for new module hbase, HBasConnectionManager empty class

---
+++ src/storage/hbase/README.rst
@@ -0,0 +1,23 @@
+HBase Storage for GeoGit
+========================
+
+This module provides support for storing the GeoGit object graph in an `HBase
+<https://hbase.apache.org/>` server.
+
+Enabling
+--------
+
+TODO
+
+Configuration
+-------------
+
+TODO
+
+Testing
+-------
+
+TODO
+
+From Mongo:
+  $ mvn test -f src/parent/pom.xml -pl :geogit-mongodb -PmongoOnlineTests
---
+++ src/storage/hbase/src/main/java/org/geogit/storage/hbase/HBaseConnectionManager.java
@@ -0,0 +1,14 @@
+/* Copyright (c) 2014 OpenPlans. All rights reserved.
+ * This code is licensed under the BSD New License, available at the root
+ * application directory.
+ */
+package org.geogit.storage.hbase;
+
+// dependency on geogit-mongodb.
+// even though ConnectionManager is only in org.geogit.storage
+import org.geogit.storage.ConnectionManager;
+
+public final class HBaseConnectionManager
+        /*extends ConnectionManager<MongoAddress, MongoClient>*/
+    {
+}
