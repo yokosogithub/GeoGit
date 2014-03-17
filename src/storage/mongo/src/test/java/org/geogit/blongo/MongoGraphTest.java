cb942a7522bb8545a61e1210a3690b9a04a5be7c
David Winslow <cdwinslow@gmail.com>
2014-03-17 16:38:10 -0400

    Add Mongo-Blueprints implementation

---
+++ src/storage/mongo/src/test/java/org/geogit/blongo/MongoGraphTest.java
@@ -0,0 +1,94 @@
+/* Copyright (c) 2013 OpenPlans. All rights reserved.
+ * This code is licensed under the BSD New License, available at the root
+ * application directory.
+ */
+package org.geogit.blongo;
+
+import java.lang.reflect.Method;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.EdgeTestSuite;
+import com.tinkerpop.blueprints.KeyIndexableGraphTestSuite;
+import com.tinkerpop.blueprints.GraphTestSuite;
+import com.tinkerpop.blueprints.GraphQueryTestSuite;
+import com.tinkerpop.blueprints.TestSuite;
+import com.tinkerpop.blueprints.VertexQueryTestSuite;
+import com.tinkerpop.blueprints.VertexTestSuite;
+import com.tinkerpop.blueprints.impls.GraphTest;
+
+public class MongoGraphTest extends GraphTest {
+    public void doTestSuite(TestSuite suite) throws Exception {
+        String test = System.getProperty("testMongoGraph");
+        if (test == null || test.equals("true")) {
+            for (Method method : suite.getClass().getDeclaredMethods()) {
+                if (method.getName().startsWith("test")) {
+                    System.out.println("Testing " + method.getName() + " ...");
+                    method.invoke(suite);
+                }
+            }
+        }
+    }
+
+    public Graph generateGraph(String directory) {
+        throw new RuntimeException("Can't create from a directory yet");
+    }
+
+    public Graph generateGraph() {
+        try {
+            MongoClient client = new MongoClient("192.168.122.165", 27017);
+            client.getDB("blueprints").dropDatabase();
+            DB db = client.getDB("blueprints");
+            DBCollection collection = db.getCollection("graph");
+            return new MongoGraph(collection);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void testVertexTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new VertexTestSuite(this));
+        printTestPerformance("VertexTestSuite", this.stopWatch());
+    }
+
+    public void testEdgeTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new EdgeTestSuite(this));
+        printTestPerformance("EdgeTestSuite", this.stopWatch());
+    }
+
+    public void testGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphTestSuite(this));
+        printTestPerformance("GraphTestSuite", this.stopWatch());
+    }
+
+    public void testEdgeQueryTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphQueryTestSuite(this));
+        printTestPerformance("GraphQueryTestSuite", this.stopWatch());
+    }
+
+    public void testVertexQueryTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new VertexQueryTestSuite(this));
+        printTestPerformance("VertexQueryTestSuite", this.stopWatch());
+    }
+
+    public void testGraphQueryTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphQueryTestSuite(this));
+        printTestPerformance("GraphQueryTestSuite", this.stopWatch());
+    }
+
+    public void testKeyIndexableGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new KeyIndexableGraphTestSuite(this));
+        printTestPerformance("GraphQueryTestSuite", this.stopWatch());
+    }
+}
